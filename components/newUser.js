8070496920b769cd18bb17f1d4ac12378b25e3ab
Henry Leung <henryleungj@gmail.com>
2024-04-19 19:11:08 -0700

    avatars made

---
+++ components/newUser.js
@@ -0,0 +1,71 @@
+import { Copy } from "lucide-react"
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@components/ui/avatar"
+ 
+import { Button } from "@components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@components/ui/dialog"
+import { Input } from "@components/ui/input"
+import { Label } from "@components/ui/label"
+
+export default function newUser(params) {
+    return(
+        <Dialog>
+            <DialogTrigger asChild>
+            <div className="jump">
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                <DialogTitle>Share link</DialogTitle>
+                <DialogDescription>
+                    Share a link!
+                </DialogDescription>
+                </DialogHeader>
+                <div className="flex items-center space-x-2">
+                <div className="grid flex-1 gap-2">
+                    <Label htmlFor="link" className="sr-only">
+                    Link
+                    </Label>
+                    <Input
+                    id="link"
+                    defaultValue="https://ui.shadcn.com/docs/installation"
+                    readOnly
+                    />
+                </div>
+                <Button type="submit" size="sm" className="px-3">
+                    <span className="sr-only">Copy</span>
+                    <Copy className="h-4 w-4" />
+                </Button>
+                </div>
+                <DialogFooter className="sm:justify-start">
+                <DialogClose asChild>
+                    <Button>
+                    Send
+                    </Button>
+                </DialogClose>
+                <DialogClose asChild>
+                    <Button type="button" variant="secondary">
+                    Close
+                    </Button>
+                </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
