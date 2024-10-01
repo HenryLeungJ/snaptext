c671de066c4dd7756e5ac45002cfc3877becd202
Henry Leung <leunghenry619@gmail.com>
2024-10-01 11:01:28 -0700

    railgun

--- src/app/api/getusers/route.js
+++ src/app/api/getusers/route.js
@@ -1,16 +1,14 @@
-
-import {dbConnect} from '@/lib/mongodb/db'
-import {NextResponse} from 'next/server'
-import User from '@/models/user'
+import { dbConnect } from "@/lib/mongodb/db";
+import { NextResponse } from "next/server";
+import User from "@/models/user";
 import { unstable_noStore as noStore } from "next/cache";
 
-
 export async function GET() {
-    noStore();
-    const con = await dbConnect();
+  noStore();
+  const con = await dbConnect();
 
-    const firstUser = await User.find();
-    // console.log(firstUser)
-    return NextResponse.json(firstUser);
-    //return NextResponse.json({message: 'Hello World'})
+  const firstUser = await User.find();
+  // console.log(firstUser)
+  return NextResponse.json(firstUser);
+  //return NextResponse.json({message: 'Hello World'})
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,42 +2,49 @@
 
 import { useEffect, useState } from "react";
 import { socket } from "../socket";
-import NewUser from '@/components/newUser';
-import Alert from '@/components/alert'
+import NewUser from "@/components/newUser";
+import Alert from "@/components/alert";
 
 export default function Home() {
-  const [allUsers, setAllUsers] = useState([])
-  const [recievedUsers, setRecievedUsers] = useState({}) //people who sent the user a message
+  const [allUsers, setAllUsers] = useState([]);
+  const [recievedUsers, setRecievedUsers] = useState({}); //people who sent the user a message
 
   function sendMessage(message, toUserId) {
     socket.emit("message-sent", message, toUserId, socket.id);
   }
 
   useEffect(() => {
-      if (socket.connected) {
+    if (socket.connected) {
       onConnect();
     }
     async function fetchUsers() {
-      await fetch("https://snaptextquick.vercel.app/api/getusers", { cache: 'no-store', headers: {
-        'Content-type': 'application/json',
-      }, method: 'GET' })
-      .then((response) => response.json())
-      .then((data) => {setAllUsers(data); console.log(data)});
+      await fetch("http://localhost:3000/api/getusers", {
+        cache: "no-store",
+        headers: {
+          "Content-type": "application/json",
+        },
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setAllUsers(data);
+          console.log(data);
+        });
     }
 
     function onConnect() {
-
       socket.on("fetchusers", () => {
-          fetchUsers();
-      })
+        fetchUsers();
+      });
       socket.on("message-from-user", (message, fromUserId) => {
-        setRecievedUsers((prev) => {return {...prev, [fromUserId]: message}}) //replaces new user who sent message with icon
-      })
-
+        setRecievedUsers((prev) => {
+          return { ...prev, [fromUserId]: message };
+        }); //replaces new user who sent message with icon
+      });
     }
     socket.on("connect", onConnect);
     socket.emit("newuser");
-    
+
     // return () => {
     //   fetchUsers()
     //   socket.emit("newuser"); //adding new user
@@ -47,37 +54,70 @@ export default function Home() {
     //   socket.off("message-from-user")
     // };
   }, []);
-  
 
   //creating texts to display
   function createText(text, index) {
-    return (<p key={index}>{text}</p>)
+    return <p key={index}>{text}</p>;
   }
 
   return (
     <div className="w-screen h-80 my-10 flex justify-center">
-    {allUsers.length == 0 ? <div className="h-full w-full flex justify-center flex-col items-center"><p className="font-bold text-2xl">Please wait for the server to start up!</p>
-    <div class="🤚">
-      <div class="👉"></div>
-      <div class="👉"></div>
-      <div class="👉"></div>
-      <div class="👉"></div>
-      <div class="🌴"></div>		
-      <div class="👍"></div>
-    </div></div> :
+      {allUsers.length == 0 ? (
+        <div className="h-full w-full flex justify-center flex-col items-center">
+          <p className="font-bold text-2xl">
+            Please wait for the server to start up!
+          </p>
+          <div class="🤚">
+            <div class="👉"></div>
+            <div class="👉"></div>
+            <div class="👉"></div>
+            <div class="👉"></div>
+            <div class="🌴"></div>
+            <div class="👍"></div>
+          </div>
+        </div>
+      ) : (
         <div className="w-[80%] grid grid-cols-4 gap-4">
           {allUsers.map((val) => {
             if (socket.id == val.userid) {
-              return <NewUser key={val.userid} id={val.userid} name={val.username} img={val.icon} highlight={true} onClick={sendMessage}/>
+              return (
+                <NewUser
+                  key={val.userid}
+                  id={val.userid}
+                  name={val.username}
+                  img={val.icon}
+                  highlight={true}
+                  onClick={sendMessage}
+                />
+              );
+            } else if (val.userid in recievedUsers) {
+              return (
+                <Alert
+                  key={val.userid}
+                  sentFrom={val.username}
+                  message={recievedUsers[val.userid]}
+                  close={() => {
+                    delete recievedUsers[val.userid];
+                    console.log(recievedUsers);
+                    setAllUsers((prev) => [...prev]);
+                  }}
+                />
+              ); //trigger rerender
             }
-            else if (val.userid in recievedUsers) {
-              return <Alert key={val.userid} sentFrom={val.username} message={recievedUsers[val.userid]} close={() => {delete recievedUsers[val.userid]; console.log(recievedUsers); setAllUsers((prev) => [...prev])}}/> //trigger rerender
-            }
-            return <NewUser key={val.userid} id={val.userid} name={val.username} img={val.icon} onClick={sendMessage}/>
+            return (
+              <NewUser
+                key={val.userid}
+                id={val.userid}
+                name={val.username}
+                img={val.icon}
+                onClick={sendMessage}
+              />
+            );
           })}
-        </div> }
+        </div>
+      )}
     </div>
   );
 }
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
